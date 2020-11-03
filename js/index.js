e0764862306e2655c241e41b1d1906a2548ba980
lifeoncode <ndu918@outlook.com>
2020-11-03 14:36:01 +0200

    local section complete

--- js/index.js
+++ js/index.js
@@ -1,29 +1,45 @@
 window.addEventListener('load', (e) => {
     
-    const [repo, branch, remote] = [document.querySelector('#repo'), document.querySelector('#branch'), document.querySelector('#remote')]
+    const [local, branch, remote] = [document.querySelector('#local'), document.querySelector('#branch'), document.querySelector('#remote')]
 
+    const [localSection, branchSection, remoteSection] = [document.querySelector('#local-content'), document.querySelector('#branch-content'), document.querySelector('#remote-content')];
 
     
-    repo.addEventListener('click', (e) => {
-        repo.classList.add('current');
+    local.addEventListener('click', (e) => {
+        local.classList.add('current');
         branch.classList.remove('current');
         remote.classList.remove('current');
+
+        // sections
+        localSection.style.display = 'block';
+        branchSection.style.display = 'none';
+        remoteSection.style.display = 'none';
     });
     
     
     
     branch.addEventListener('click', (e) => {
         branch.classList.add('current');
-        repo.classList.remove('current');
+        local.classList.remove('current');
         remote.classList.remove('current');
+
+        // sections
+        localSection.style.display = 'none';
+        branchSection.style.display = 'block';
+        remoteSection.style.display = 'none';
     });
 
     
     
     remote.addEventListener('click', (e) => {
         remote.classList.add('current');
-        repo.classList.remove('current');
+        local.classList.remove('current');
         branch.classList.remove('current');
+
+        // sections
+        localSection.style.display = 'none';
+        branchSection.style.display = 'none';
+        remoteSection.style.display = 'block';
     });
     
 
